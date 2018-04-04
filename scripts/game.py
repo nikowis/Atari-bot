3c9fc39a01ce89fc02566c22abfc589d2787c005
Nikodem <wisniewskin@student.mini.pw.edu.pl>
2018-04-04 11:38:35 +0200

    Sample screens and preprocessing frame

--- scripts/game.py
+++ scripts/game.py
@@ -1,5 +1,20 @@
 # Import the gym module
 import gym
+import numpy as np
+import tensorflow as tf
+import scipy.misc
+
+def to_grayscale(img):
+    return np.mean(img, axis=2).astype(np.uint8)
+
+
+def downsample(img):
+    return img[::2, ::2]
+
+
+def preprocess(img):
+    return to_grayscale(downsample(img))
+
 
 # Create a breakout environment
 env = gym.make('BreakoutDeterministic-v4')
@@ -10,7 +25,11 @@ env.render()
 
 is_done = False
 while not is_done:
-  # Perform a random action, returns the new frame, reward and whether the game is over
-  frame, reward, is_done, _ = env.step(env.action_space.sample())
-  # Render
-  env.render()
+    # Perform a random action, returns the new frame, reward and whether the game is over
+    frame, reward, is_done, _ = env.step(env.action_space.sample())
+
+    frame = preprocess(frame)
+
+    #Render
+    if not is_done:
+        env.render()
